cb0c8c483b1c5535238611e5cb477a1c26823344
Artm1n3r <budn1k0ver@gmail.com>
2025-05-30 23:15:15 +0300

    just start

---
+++ src/main/java/xyz/opsf/webcore/processor/WebCoreAnnotationProcessor.java
@@ -0,0 +1,17 @@
+package main.java.xyz.opsf.webcore.processor;
+
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.TypeElement;
+
+public class WebCoreAnnotationProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'process'");
+    }
+
+}
